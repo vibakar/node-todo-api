b31ed443619d5156168d975bb0a6a464cd5379ac
vibakar <vibakar23@gmail.com>
2016-12-30 17:17:13 +0530

    update commit

---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+const {MongoClient,ObjectID}=require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('Unable to connect to mongodb server',err);
+  }
+  console.log('Connected to mongodb server');
+
+//findOneAndUpdate
+
+// db.collection("Todo").findOneAndUpdate({
+//   _id:new ObjectID("5865deafb480752948601ae7")
+// },{
+//   $set:{
+//     complete:false
+//   }
+// },{
+//   returnOriginal:false
+// }).then((result)=>{
+//   console.log(result);
+// });
+
+  db.collection("Users").findOneAndUpdate({
+    name:"viba"
+  },{
+    $inc:{
+      age:5
+    }
+  },{
+    returnOriginal:false
+  }).then((result)=>{
+    console.log(result);
+  });
+  // db.close();
+});
